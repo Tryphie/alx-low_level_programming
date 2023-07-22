53e631d7845308a54a8e539f19ebac6d16ef5b04
Tryphie <tryphi3@gmail.com>
2023-07-22 09:47:51 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,21 +12,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-
 	if (size > 0)
-
 	{
-
 		if (array != NULL && cmp != NULL)
-
 		{
 			while (i < size)
-
 			{
-
 				if (cmp(array[i]))
 					return (i);
-			i++;
+				i++;
 			}
 		}
 	}
